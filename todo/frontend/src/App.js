50971562b14947ec76c4dc1f63a0d6948c83147c
Vitaliy <vsssv@ya.ru>
2021-11-21 18:42:19 +0300

    Lesson_2

---
+++ todo/frontend/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import UserList from "./components/User";
+import Footer from "./components/Footer";
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NaviBar from "./components/Menu";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
+        }
+    }
+    componentDidMount(){
+        axios.get('http://127.0.0.1:8000/api/users/').then(
+            response =>{
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            }
+        ).catch(error => console.log(error))
+    }
+    render(){
+        return (
+            <div>
+                <NaviBar />
+                <UserList users={this.state.users} />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
